7c3396d33ef313426e982ad7a3f63e431b65f895
dgarciavaleriano <dgarciavaleriano@gmail.com>
2026-07-12 09:48:45 +0200

    Add health check, logger and environment variables

---
+++ src/config/logger.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+import logging
+
+load_dotenv()
+
+logging.basicConfig(
+    level=os.getenv("LOG_LEVEL", "INFO "),
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+class Logger:
+    @staticmethod
+    def get_logger(name: str) -> logging.Logger:
+        return logging.getLogger(name)
