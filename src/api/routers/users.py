97ed5570a09e92b3e564b5c3314d4c43c5c843a3
Marco Memba <marcomemba@outlook.es>
2026-07-19 13:45:21 +0200

    Primera implementacion de endpoints de usuarios para creacion y borrado.

--- src/api/routers/users.py
+++ src/api/routers/users.py
@@ -1,7 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+
+from models.user import UserCreate
 
 router: APIRouter = APIRouter()
 
-@router.get("/")
-async def hello_world():
-    return "Hello from administracioncentral!"
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserCreate):
+    # Logica de creacion de usuario
+    return {
+        "message": "Endpoint de creacion de usuario",
+        "user": user,
+    }
+
+
+@router.delete("/{user_id}", status_code=status.HTTP_200_OK)
+async def delete_user(user_id: str):
+
+    # Logica de borrado de usuario
+    return {
+        "message": "Endpoint de borrado de usuario",
+        "user_id": user_id,
+    }
